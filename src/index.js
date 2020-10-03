9c3df0739cc213213d82f891a4566fd51233ba7d
chexovpert <70890731+chexovpert@users.noreply.github.com>
2020-10-03 16:08:59 +0300

    init: create index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,51 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (typeof array=="undefined") {
+        a=0;
+        return a;
+    }
+    var a=0;
+    array.forEach(element => {
+        if (element<a) {
+            a=element
+        }
+       /* else 
+        a=0;*/
+    }
+    
+    );
+  return a;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (typeof array=="undefined") {
+        a=0;
+        return a;
+    }
+    var a=0;
+    array.forEach(element => {
+        if (element>a) {
+            a=element
+        }
+    }
+    )
+    
+return a;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (typeof array=="undefined") {
+        a=0;
+        return a;
+    }
+    
+    if (array.length!=0) {
+        var count=array.length;
+    var avrg=array.reduce(function(sum,current) {
+       
+       return sum+current;
+    },0)
+    return a=avrg/count;
+}
+  return a;
 }
